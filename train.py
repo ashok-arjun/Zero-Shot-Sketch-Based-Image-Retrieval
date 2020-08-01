e7bf02d535efb300e668df56fa395ed27e0b3254
Arjun Ashok <arjun2000ashok@gmail.com>
2020-08-01 15:49:19 +0530

    Update train.py

--- train.py
+++ train.py
@@ -21,8 +21,8 @@ from evaluate import evaluate
 from utils import *
 
 class Trainer():
-  def __init__(self, data_dir, dataloaders):
-    self.dataloaders = dataloaders # temporary, to save time
+  def __init__(self, data_dir):
+    self.dataloaders = Dataloaders(data_dir)
     self.train_dict = self.dataloaders.train_dict
     self.test_dict = self.dataloaders.test_dict
   
